9da782889805eb3ca447db26a486233f854cbaa6
Zamir <argashokovzamir@gmail.com>
2021-06-15 19:39:41 +0200

    Initial Commit

---
+++ MyToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by Zamir Argashokov on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
